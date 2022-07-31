8948d491e966c9cd56eb57bc7d57e71ca18ee71e
VladimirPariy <Vladimir.pariy@gmail.com>
2022-07-31 17:28:11 +0300

    added filtration by search

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,15 +4,18 @@ import {List} from '../components/List';
 import {Card} from '../components/Card';
 import {Controls} from '../components/Controls';
 import {useDispatch, useSelector} from "react-redux";
-import {selectAllCountries, selectCountriesInfo} from "../store/countries/countries-selectors";
+import {selectAllCountries, selectCountriesInfo, selectVisibleCountries} from "../store/countries/countries-selectors";
 import {useEffect} from "react";
 import {loadCountries} from "../store/countries/countries-actions";
+import {selectSearch} from "../store/controls/controls-selector";
 
 export const HomePage = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
-    const countries = useSelector(selectAllCountries)
+    const search = useSelector(selectSearch)
+    const countries = useSelector(state => selectVisibleCountries(state, {search}))
     const {status, error, qty} = useSelector(selectCountriesInfo)
+
 
     useEffect(() => {
         if(!qty) {
