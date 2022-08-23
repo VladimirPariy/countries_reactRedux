22d8de709bddd1f3150cd521fd85ad853496e8de
VladimirPariy <Vladimir.pariy@gmail.com>
2022-08-24 00:32:12 +0300

    refactoring theme by slice

---
+++ src/features/theme/theme-slice.js
@@ -0,0 +1,1 @@
+import {createSlice} from "@reduxjs/toolkit";const themeSlice = createSlice({    name: "@@theme",    initialState: 'light',    reducers: {        setTheme: (_, action) => action.payload,    }})export const {setTheme} = themeSlice.actions;export const themeReducer = themeSlice.reducer
